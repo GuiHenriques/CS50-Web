b47c7a9543d14ada45533287ddd23db5457e7d23
GuiHenriques <gui10ghc@gmail.com>
2023-10-26 01:03:24 -0300

    probably last commit

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.index, name="index"),
-    path("new", views.new, name="new"),
     path("edit", views.edit, name="edit"),
+    path("new", views.new, name="new"),
+    path("random", views.random, name="random"),
     path("<str:name>", views.entry, name="entry"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,54 +1,58 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.http import HttpResponse
 
+from random import choice
 import markdown2
 from . import util
+
+
+def get_normalized_entry_from_post(request):
+    title = request.POST.get("title")
+    content = request.POST.get("content").replace('\r\n', '\n')
+    return title, content
 
 def index(request):
     query = request.GET.get("q")
     if query:
-        if util.get_entry(query):
-            return entry(request, query)
-        else:
-            entries = util.list_entries()
-            entries = [e for e in entries if query.lower() in e.lower()]
-            return render(request, "encyclopedia/search.html", {
-                "entries": entries
-            })
-    else:
-        return render(request, "encyclopedia/index.html", {
-            "entries": util.list_entries(),
-        })
+        entry = util.get_entry(query)
+        if entry:
+            return redirect("entry", name=query)
+        entries = [e for e in util.list_entries() if query.lower() in e.lower()]
+        return render(request, "encyclopedia/search.html", {"entries": entries})
+    
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries(),
+    })
 
 def entry(request, name):
+    if request.method == "POST":
+        title, content = get_normalized_entry_from_post(request)
+        util.save_entry(title, content)
+        return redirect("entry", name=title)
+    
     md = util.get_entry(name)
     if md:
         html = markdown2.markdown(md)
         return render(request, "encyclopedia/entry.html", {
-            "html": html,
+            "content": html,
             "title": name.capitalize(),
         })
-    else:
-        return render(request, "encyclopedia/error.html", {
-            "message": f"Entry for '{name}' not found.",
-            "title": name,
-        })
+
+    return render_error(request, f"Entry for '{name}' not found.", name)
 
 def new(request):
     if request.method == "POST":
-        title = request.POST.get("title").capitalize()
+        title, content = get_normalized_entry_from_post(request)
+        title = title.capitalize()
         if title in util.list_entries():
-            return render(request, "encyclopedia/error.html", {
-                "message": f"The entry '{title}' already exists.",
-                "title": "Error"
-            })
-        content = request.POST.get("content")
+            return render_error(request, f"The entry '{title}' already exists.")
+        
         util.save_entry(title, content)
-        return entry(request, title)
+        return redirect("entry", name=title)
 
     return render(request, "encyclopedia/new.html", {
-        "method": request.method
+        "method": request.method,
     })
 
 def edit(request):
@@ -56,5 +60,15 @@ def edit(request):
     md = util.get_entry(title)
     return render(request, "encyclopedia/edit.html", {
         "content": md,
-        "title": title
+        "title": title,
+    })
+
+def random(req):
+    title = choice(util.list_entries())
+    return redirect("entry", name=title)
+
+def render_error(request, message, title="Error"):
+    return render(request, "encyclopedia/error.html", {
+        "message": message,
+        "title": title,
     })
